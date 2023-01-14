fab0fbfaa0a90699e4622b483ef4ab3e72a3044c
milenaalexandra874 <alexandramunguia20@gmail.com>
2023-01-14 16:28:14 -0500

    functions and data was added

---
+++ src/app.js
@@ -0,0 +1,21 @@
+let weather={
+"apiKey":"62282d631020ae75d9cd9f9ee3a78a2e"
+fetchWeather: function (city){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="
+    +city
+    "&units=metric&appid="
+    +this.apiKey
+    )
+.then((responce)=>responce.json());
+.then((data)=> this.displayWeather(data));
+}
+displayWeather: function(data){
+    const{name} = data;
+    const{icons, discription}= data.weather[0];
+    const{temp, humidity}= data.main;
+    const{speed}=data.wind;
+    console.log(name,icon,discription,temp,humidity,speed)
+    document.querySelector(".city").innerText="+ name";
+    document.querySelector("+icons").src=""
+}
+}
